515f62391f0144c5a54cd904d95d85df4fbcea91
Hrafnhildur Arna Nielsen <han7@hi.is>
2019-03-21 16:13:12 +0000

    mimim

--- einingaprofun.ut.py
+++ einingaprofun.ut.py
@@ -11,7 +11,7 @@ class TestSpurningaleikur(unittest.TestCase):
         self.func= Question(1,0)"""
 
     def test_2(self):
-        self.assertEqual(quest.leikmadur, 1)
+        self.assertEqual(quest.leikmadur, 0)
 
 if __name__ == '__main__':
     unittest.main()
